19319eef2be165594f874a3b64b0f8a17df07a68
JayoonKim <jayoon999@gmail.com>
2024-04-11 16:18:20 +0900

    fix: pages conflict 해결

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-import React from 'react';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
