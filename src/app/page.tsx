1ffc375b2a201a1c8057117a918590fe795d8588
JayoonKim <jayoon999@gmail.com>
2024-04-11 15:20:18 +0900

    feat: map 구현 (NaverMapAPI)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import styled from 'styled-components';
+import Link from 'next/link';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import RootLayout from './layout';
 
 const Container = styled.div`
   background-color: red;
@@ -8,5 +12,10 @@ const Container = styled.div`
 `;
 
 export default function Home() {
-  return <Container> HI!! </Container>;
+  return (
+    <Container>
+      HI!!
+      <Link href="/map">map</Link>
+    </Container>
+  );
 }
