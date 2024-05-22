6e86ad6349e40b551bf865cba8b3461a1e72138d
JayoonKim <jayoon999@gmail.com>
2024-05-22 18:39:18 +0900

    feat: Contract Form Page- 계약서 초안 구현

--- src/components/contract/contractForm.tsx
+++ src/components/contract/contractForm.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+
 // 새로고침 대비 localstorage
-// modal로 개발
+// 채팅창 내부에서 modal 로 띄워질 예정.
+
+interface InputProps {
+  size?: string;
+}
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
   margin: 0 auto;
-  width: 800px;
+  width: 80vw;
   line-height: 30px;
   margin-top: 40px;
+  border-radius: 10px;
+  background-color: aliceblue;
 `;
 const ContractBody = styled.div`
   margin: 0 auto;
 `;
 const Title = styled.h1`
   font-size: 24px;
-  margin: 0 auto;
   /* justify-content: center; */
 `;
 
@@ -33,18 +39,26 @@ const Declaration = styled.div`
   border: solid 1.5px black;
   border-radius: 10px;
   padding: 17px;
-  /* margin: 10px; */
-  width: 570px;
+  margin: 0 auto;
+  width: 80%;
 `;
-const Information = styled.div`
-  /* float: left; */
-  /* margin-right: 500px; */
-`;
-const Input = styled.input`
+
+// props 로 길이도 받아서 input 길이 조절할 수 있게 수정하기
+
+const Input = styled.input<InputProps>`
   border: none;
   border-bottom: solid 1px black;
   outline: none;
   width: 80px;
+  width: ${(props) => {
+    if (props.size === 'small') {
+      return '40px';
+    }
+    if (props.size === 'big') {
+      return '200px';
+    }
+    return '100px'; // default 값
+  }};
   margin-left: 5px;
   padding-left: 6px;
   /* margin: 5px; */
@@ -127,33 +141,92 @@ function ContactForm() {
           </div>
           은 아래와 같이 임대차 계약을 체결한다.
         </Declaration>
-        <Information>
-          <div>1. 임차주택의 표시</div>
-          <div>
-            <Label>소재지</Label>
-            <Input
-              name="address"
-              placeholder="도로명주소"
-              onChange={addressReg.onChange}
-              onBlur={addressReg.onBlur}
-              ref={addressReg.ref}
-            />
-          </div>
-          <div>
-            <Label>면적</Label>
-            <Input name="area" />
-          </div>
-          <div>
-            <Label>계약조건 보증금</Label>
-            <Input name="deposit" type="text" placeholder="보증금 입력" />
-          </div>
-          <div>
-            <Label>월차임료</Label>
-            <Input name="monthlyRent" type="text" placeholder="월차임료 입력" />
-          </div>
-        </Information>
-
+        <div>1. 부동산의 표시</div>
+        <div>
+          <Label>소재지</Label>
+          <Input
+            name="address"
+            placeholder="도로명주소"
+            onChange={addressReg.onChange}
+            onBlur={addressReg.onBlur}
+            ref={addressReg.ref}
+          />
+        </div>
+        <div>
+          <Label>면적</Label>
+          <Input name="area" />
+          {'m\xB2'}
+        </div>
+        <div>
+          <Label>월세(전세) 보증금</Label>
+          <Input name="deposit" type="text" placeholder="보증금 입력" />
+        </div>
+        <div>
+          <Label>월 차임료</Label>
+          <Input name="monthlyRent" type="text" placeholder="월차임료 입력" />
+        </div>
+        <div>2. 계약 내용</div>
+        <div>
+          제1조(목적) 위 부동산의 임대차에 한하여 임대인과 임차인은 합의에 의해
+          임차보증금 및 차임을 아래와 같이 지불하기로 한다.
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td>계약금</td>
+              <td>
+                <Input />
+                원정은 계약시 지불하고,
+              </td>
+            </tr>
+            <tr>
+              <td>중도금</td>
+              <td>
+                <Input />
+                원정은 <Input size="small" />년 <Input size="small" />월{' '}
+                <Input size="small" />
+                일에 지불하며,
+              </td>
+            </tr>
+            <tr>
+              <td>잔금</td>
+              <td>
+                <Input />
+                원정은 <Input size="small" />년 <Input size="small" />월{' '}
+                <Input size="small" />
+                일에 지불한다.
+              </td>
+            </tr>
+            <tr>
+              <td>차임</td>
+              <td>
+                <Input />
+                원정은 매월 <Input size="small" />
+                일에 지불한다.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          {' '}
+          (입금 계좌 : <Input size="big" /> )
+        </div>
+        <div>제2조(임대차 기간)</div>
+        <div>
+          임대차 기간은 <Input size="small" />년 <Input size="small" />월
+          <Input size="small" />
+          일부터 <Input size="small" />년 <Input size="small" />
+          월 <Input size="small" />
+          일까지 <Input size="small" />
+          개월간으로 한다.
+        </div>
+        {/* 사인하기 하고 돌아오면 계약서보내기 활성화 */}
         <SubmitButton type="submit">계약서 보내기</SubmitButton>
+
+        <SubmitButton type="submit">
+          사인하기
+          {/* <Link href={'/sign'}>사인하기</Link> */}
+        </SubmitButton>
       </ContractBody>
     </FormContainer>
   );
