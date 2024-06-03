1baa300a63b599cdb83d743a0b6f1d4e61b9214e
kimhongrae <khr0690@hanmail.net>
2024-06-03 16:10:27 +0900

    feat: 마이페이지 ui

--- src/app/mypage/contract/page.tsx
+++ src/app/mypage/contract/page.tsx
@@ -1,4 +1,16 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 function ContractPage() {
-  return <div>이게 계약 페이지가 뜨는 채팅 있을 곳이고</div>;
+  const router = useRouter();
+  return (
+    <button
+      type="button"
+      onClick={() => router.push('/contract', { scroll: false })}
+    >
+      계약서 띄우기›
+    </button>
+  );
 }
 export default ContractPage;
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,25 +1,50 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 1;
+`;
+
+const Text = styled.h1`
+  text-align: center;
+`;
+
+function MyPage() {
+  return (
+    <Container>
+      <Main>
+        <Text>마이페이지</Text>
+      </Main>
+    </Container>
+  );
+}
+
+export default MyPage;
+
 // import Link from 'next/link';
 // import ContractForm from '@/components/contract/contractForm';
 
 // Mypage -> 채팅 modal-> 계약서 modal
 // 채팅 부분이 아직 되지 않아서 일단 계약서부터 개발 시작함.
 
-function MyPage() {
-  const router = useRouter();
-  return (
-    <button
-      type="button"
-      onClick={() => router.push('/contract', { scroll: false })}
-    >
-      계약서 띄우기›
-    </button>
-  );
-}
+// function MyPage() {
+//   const router = useRouter();
+//   return (
+//     <button
+//       type="button"
+//       onClick={() => router.push('/contract', { scroll: false })}
+//     >
+//       계약서 띄우기›
+//     </button>
+//   );
+// }
 
-export default MyPage;
+// export default MyPage;
 
 //   {/* <button>
 //   <Link href="/modal">계약하러가기</Link>
